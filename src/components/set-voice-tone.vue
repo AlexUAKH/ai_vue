c080444da9127b42ac5879c2d06cac8b3e9c56fb
AlexUAKH <sashaos1404@gmail.com>
2021-02-26 21:23:20 +0200

    set-voice-tone

---
+++ src/components/set-voice-tone.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="setVoiceTone">
+    <ul>
+      <li>
+        <span
+          ><img src="@/assets/images/ic_voiceTone.svg" alt="Voice Tone" />Voice
+          Tone</span
+        >
+        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+          <label class="btn active">
+            <input type="radio" name="setVoiceTone" id="High" />High
+          </label>
+          <label class="btn">
+            <input type="radio" name="setVoiceTone" id="Low" checked />Low
+          </label>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/components/setting-panel.vue
+++ src/components/setting-panel.vue
@@ -60,32 +60,16 @@
 
     <hr />
 
-    <div class="setVoiceTone">
-      <ul>
-        <li>
-          <span
-            ><img src="images/ic_voiceTone.svg" alt="Voice Tone" />Voice
-            Tone</span
-          >
-          <div class="btn-group btn-group-toggle" data-toggle="buttons">
-            <label class="btn active">
-              <input type="radio" name="setVoiceTone" id="High" />High
-            </label>
-            <label class="btn">
-              <input type="radio" name="setVoiceTone" id="Low" checked />Low
-            </label>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <set-voice-tone></set-voice-tone>
   </section>
 </template>
 
 <script>
 import SetLangPanel from "./set-lang-panel.vue";
 import setSoundMode from "./set-sound-mode.vue";
+import SetVoiceTone from "./set-voice-tone.vue";
 export default {
-  components: { setSoundMode, SetLangPanel },
+  components: { setSoundMode, SetLangPanel, SetVoiceTone },
   data: () => ({
     isSettingsHided: false,
     windowWidth: window.innerWidth
