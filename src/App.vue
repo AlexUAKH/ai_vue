71a5db98b04fc548009f134eb661d68f9cbaf4a8
AlexUAKH <sashaos1404@gmail.com>
2021-02-26 19:32:41 +0200

    setting panel

--- src/App.vue
+++ src/App.vue
@@ -13,140 +13,7 @@
         <select-lang-list></select-lang-list>
         <!-- /END #selectLangList -->
 
-        <section id="settingPanel" class="settingPanel">
-          <button class="settingPanelBtn"></button>
-          <div class="setExportServices">
-            <ul>
-              <li id="exportToZoom">
-                <p><span class="ic ic_zoom"></span>zoom に出力</p>
-                <div class="custom-switch custom-switch-label-io">
-                  <input
-                    class="custom-switch-input"
-                    id="export_zoom"
-                    type="radio"
-                    name="setExportServices"
-                  />
-                  <label class="custom-switch-btn" for="export_zoom"></label>
-                </div>
-              </li>
-              <li id="exportToTeams">
-                <p><span class="ic ic_teams"></span>Teams に出力</p>
-                <div class="custom-switch custom-switch-label-io">
-                  <input
-                    class="custom-switch-input"
-                    id="export_teams"
-                    type="radio"
-                    name="setExportServices"
-                  />
-                  <label class="custom-switch-btn" for="export_teams"></label>
-                </div>
-              </li>
-              <li id="exportToHangouts">
-                <p><span class="ic ic_hangouts"></span>Hangouts に出力</p>
-                <div class="custom-switch custom-switch-label-io">
-                  <input
-                    class="custom-switch-input"
-                    id="export_hangouts"
-                    type="radio"
-                    name="setExportServices"
-                  />
-                  <label
-                    class="custom-switch-btn"
-                    for="export_hangouts"
-                  ></label>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <hr />
-          <div class="setSoundMode">
-            <ul>
-              <li>
-                <p><span class="ic ic_lipSync"></span>音声限定モード</p>
-                <div class="custom-switch custom-switch-label-io">
-                  <input
-                    class="custom-switch-input"
-                    id="soundMode"
-                    type="checkbox"
-                  />
-                  <label class="custom-switch-btn" for="soundMode"></label>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <hr />
-          <div class="setLang">
-            <ul>
-              <li>
-                <span class="title"
-                  ><img
-                    src="images/ic_comment.svg"
-                    alt="発話言語"
-                  />発話言語</span
-                >
-                <div class="selectdiv">
-                  <select>
-                    <option>English</option>
-                    <option>Spanish</option>
-                    <option>Korean</option>
-                    <option>Mandarin</option>
-                    <option>Cantonese</option>
-                    <option>Taiwanese</option>
-                    <option>Vietnamese</option>
-                    <option>French</option>
-                    <option>Italiano</option>
-                  </select>
-                </div>
-              </li>
-              <li>
-                <span class="title"
-                  ><img
-                    src="images/ic_translate.svg"
-                    alt="通訳言語の追加"
-                  />通訳言語の追加</span
-                >
-                <div class="selectdiv">
-                  <select>
-                    <option selected>訳文の言語</option>
-                    <option>English</option>
-                    <option>Spanish</option>
-                    <option>Korean</option>
-                    <option>Mandarin</option>
-                    <option>Cantonese</option>
-                    <option>Taiwanese</option>
-                    <option>Vietnamese</option>
-                    <option>French</option>
-                    <option>Italiano</option>
-                  </select>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <hr />
-          <div class="setVoiceTone">
-            <ul>
-              <li>
-                <span
-                  ><img src="images/ic_voiceTone.svg" alt="Voice Tone" />Voice
-                  Tone</span
-                >
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                  <label class="btn active">
-                    <input type="radio" name="setVoiceTone" id="High" />High
-                  </label>
-                  <label class="btn">
-                    <input
-                      type="radio"
-                      name="setVoiceTone"
-                      id="Low"
-                      checked
-                    />Low
-                  </label>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
+        <setting-panel></setting-panel>
         <!-- /END #settingPanel -->
       </div>
       <!-- /END #contents -->
@@ -520,13 +387,15 @@
 import AiNavigation from "./components/ai-navigation.vue";
 import AvailableTime from "./components/available-time.vue";
 import SelectLangList from "./components/select-lang-list.vue";
+import SettingPanel from "./components/setting-panel.vue";
 import TextList from "./components/text-list.vue";
 export default {
   components: {
     AiNavigation,
     AvailableTime,
     TextList,
-    SelectLangList
+    SelectLangList,
+    SettingPanel
   }
 };
 </script>
